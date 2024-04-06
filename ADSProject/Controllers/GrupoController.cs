d61f120ba20aad5d87a517c38effed9d057160f9
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-04-05 22:01:03 -0600

    Se agrego CRUD de grupos

---
+++ ADSProject/Controllers/GrupoController.cs
@@ -0,0 +1,138 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADSProject.Controllers
+{
+    [Route("api/grupo/")]
+    public class GrupoController : ControllerBase
+    {
+        private readonly IGrupo Grupo;
+        private const string COD_EXITO = "000000";
+        private const string COD_ERROR = "999999";
+        private string pCodRespuesta;
+        private string pMensajeUsuario;
+        private string pMensajeTecnico;
+
+        public GrupoController(IGrupo Grupo)
+        {
+            this.Grupo = Grupo;
+        }
+        [HttpPost("agregarGrupo")]
+        public ActionResult<string> AgregarGrupo([FromBody] Grupo Grupo)
+        {
+            try
+            {
+                int contador = this.Grupo.AgregarGrupo(Grupo);
+
+                if (contador > 0)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro insertado con exito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                else
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Ocurrio un problema al insertar el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost("actualizarGrupo/{idGrupo}")]
+        public ActionResult<string> ActualizarGrupo(int idGrupo, [FromBody] Grupo Grupo)
+        {
+            try
+            {
+                int contador = this.Grupo.ActualizarGrupo(idGrupo, Grupo);
+
+                if (contador > 0)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro acuatilzado con exito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpDelete("eliminarGrupo/{idGrupo}")]
+        public ActionResult<string> EliminarGrupo(int idGrupo)
+        {
+            try
+            {
+                bool eliminado = this.Grupo.EliminarGrupo(idGrupo);
+
+                if (eliminado)
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Registro eliminado con exito";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+                else
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "Ocurrio un problema al eliminar el registro";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+                }
+
+                return Ok(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet("obtenerGrupoPorID/{idGrupo}")]
+        public ActionResult<string> ObtenerGrupoPorID(int idGrupo)
+        {
+            try
+            {
+                Grupo Grupo = this.Grupo.ObtenerGruposPorID(idGrupo);
+
+                if (Grupo != null)
+                {
+                    return Ok(Grupo);
+                }
+                else
+                {
+                    pCodRespuesta = COD_EXITO;
+                    pMensajeUsuario = "No se encontraron datos de la Grupo";
+                    pMensajeTecnico = pCodRespuesta + " || " + pMensajeUsuario;
+
+                    return NotFound(new { pCodRespuesta, pMensajeUsuario, pMensajeTecnico });
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet("obtenerGrupo")]
+        public ActionResult<List<Estudiante>> ObtenerGrupo()
+        {
+            try
+            {
+                List<Grupo> lstGrupo = this.Grupo.ObtenerTodosLosGrupos();
+
+                return Ok(lstGrupo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ADSProject/Repositories/GrupoRepository.cs
@@ -0,0 +1,93 @@
+﻿using ADSProject.Interfaces;
+using ADSProject.Models;
+
+namespace ADSProject.Repositories
+{
+    public class GrupoRepository : IGrupo
+    {
+        private List<Grupo> lstGrupo = new List<Grupo>
+        {
+            new Grupo {IdGrupo = 1, IdCarrera = 1, IdMateria = 1, IdProfesor = 1
+            , Ciclo = 0124, Anio = 2024}
+        };
+
+        public int ActualizarGrupo(int idGrupo, Grupo Grupo)
+        {
+            try
+            {
+                int indice = lstGrupo.FindIndex(tmp => tmp.IdGrupo == idGrupo);
+
+                lstGrupo[indice] = Grupo;
+                lstGrupo[indice].IdGrupo = idGrupo;
+
+                return idGrupo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int AgregarGrupo(Grupo Grupo)
+        {
+            try
+            {
+                if (lstGrupo.Count > 0)
+                {
+                    Grupo.IdGrupo = lstGrupo.Last().IdGrupo + 1;
+                }
+                lstGrupo.Add(Grupo);
+
+                return Grupo.IdGrupo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool EliminarGrupo(int idGrupo)
+        {
+            try
+            {
+                int indice = lstGrupo.FindIndex(tmp => tmp.IdGrupo == idGrupo);
+
+                lstGrupo.RemoveAt(indice);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Grupo ObtenerGruposPorID(int idGrupo)
+        {
+            try
+            {
+                Grupo Grupo = lstGrupo.FirstOrDefault(tmp => tmp.IdGrupo == idGrupo);
+
+                return Grupo;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Grupo> ObtenerTodosLosGrupos()
+        {
+            try
+            {
+                return lstGrupo;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
