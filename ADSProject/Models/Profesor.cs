91c93f6f8e6a6b60086be83466e01c1291c8340b
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-04-13 12:02:16 -0600

    se agregaron validaciones para los modelos Carrera, Profesor y Grupo

--- ADSProject/Models/Profesor.cs
+++ ADSProject/Models/Profesor.cs
@@ -1,10 +1,17 @@
-﻿namespace ADSProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADSProject.Models;
+
+public class Profesor
 {
-    public class Profesor
-    {
-        public int IdProfesor { get; set; }
-        public string NombreProfesor { get; set; }
-        public string ApellidoProfesor { get; set; }
-        public string Email {  get; set; }
-    }
+    public int IdProfesor { get; set; }
+    [Required(ErrorMessage = "Este es un campo requerido")]
+    [MaxLength(length: 50, ErrorMessage = "la longitud del campo no puede ser mayor a 50 caracteres")]
+    public string NombreProfesor { get; set; }
+    [Required(ErrorMessage = "Este es un campo requerido")]
+    [MaxLength(length: 50, ErrorMessage = "La longitud del campo no puede ser mayor a 50 caracteres")]
+    public string ApellidoProfesor { get; set; }
+    [Required(ErrorMessage = "Este es un campo requerido")]
+    [MaxLength(length: 254, ErrorMessage = "La longitud del campo no puede ser mayor a 254 caracteres")]
+    public string Email {  get; set; }
 }
