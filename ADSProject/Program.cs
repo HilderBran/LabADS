d2b563c2ab88657a7a70a8b2be76d48a253efa56
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-05-11 12:20:28 -0600

    Configuracion CORS

--- ADSProject/Program.cs
+++ ADSProject/Program.cs
@@ -1,6 +1,7 @@
 using ADSProject.Database;
 using ADSProject.Interfaces;
 using ADSProject.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +12,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Inyeccion de dependencias
 builder.Services.AddScoped<IEstudiante, EstudianteRepository>();
 builder.Services.AddScoped<ICarreras, CarreraRepository>();
 builder.Services.AddScoped<IGrupo, GrupoRepository>();
 builder.Services.AddScoped<IProfesor, ProfesorRepository>();
 builder.Services.AddScoped<IMateria, MateriaRepository>();
 builder.Services.AddDbContext<ApplicationDBContext>();
+
+//Configurando CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(configuration =>
+    {
+        configuration.WithOrigins(builder.Configuration["allowedOrigins"]!)
+        .AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 
 var app = builder.Build();
 
@@ -26,6 +39,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
